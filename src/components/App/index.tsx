4368f7c59c836ebf22ba13de2cb72525f42620be
Erick Gannem <erickgannem@gmail.com>
2021-06-08 05:43:51 -0400

    some layout changes and scrollbar

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import styled from 'styled-components'
 
 import Navbar from '../Navbar'
 import PageRouter from '../PageRouter'
 
-const _App = styled.div`
-  display: grid;
-  grid-template-rows: 0fr 1fr;
-  height: 100%;
-`
-
 export default function App () {
   return (
-    <_App>
       <Router>
         <Navbar />
         <PageRouter />
       </Router>
-    </_App>
   )
 }
