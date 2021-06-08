08c9fef606b27b53d0f9b2744cd7d0f5f627936b
Erick Gannem <erickgannem@gmail.com>
2021-06-08 13:13:23 -0400

    setting up new layout

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 
-import guidelines from '../../guidelines'
-
-const { sizes } = guidelines
-
 const Container = styled.div`
-  display: grid;
-  padding: ${sizes.rem['30px']}rem;
 `
 
 export default Container
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import Container from '../../components/Container'
+import Column from '../../components/Column'
+import Row from '../../components/Row'
 
 export default function Home () {
   return (
     <Container>
+      <Column>
+        <Row>
 
+        </Row>
+      </Column>
     </Container>
   )
 }
