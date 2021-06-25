58656b2be2b3f58d8e8abea704c1aec8a8f781ff
Erick Gannem <erickgannem@gmail.com>
2021-06-25 05:29:26 -0400

    wrap item description. Set margins on it

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -29,6 +29,10 @@ background-color: ${colors.darkBlueGreen90};
 border-radius: 5px;
 box-shadow: 1px -1px 10px ${colors.shadow60};
 
+@media (max-width: 449px) {
+  max-width: 75%;
+  min-width: 75%;
+}
 @media (max-width: 899px) {
   margin: 1.5rem 0 0 0;
 }
@@ -65,11 +69,22 @@ const _ItemTitle = styled.h1`
   color: ${colors.coolGrey};
   font-weight: 400;
   font-family: 'Fira Sans';
+  line-break: loose;
   
   @media (max-width: 449px) {
     text-align: center;
     font-size: 1.5rem;
-    padding: 1rem;
+  }
+`
+
+const _ItemDescription = styled.p`
+  color: ${colors.coolGrey};
+  margin: 1rem 0;
+`
+
+const _LowerItemSection = styled.div`
+  @media (max-width: 449px) {
+    padding: 1.2rem;
   }
 `
 // Will be separated
@@ -80,42 +95,52 @@ export default function Works () {
       <_ImageGroup>
         <_GroupItem>
           <Image src={Placeholder} width='100%' borderRadius='5px 5px 0 0'/>
-          <_ItemTitle>
-            {'Clicksign Asaas Integration'}
-          </_ItemTitle>
-          <_Technologies>
-            <_Technology>
-              <DiNodejsSmall size='2.3rem' color={colors.coolGrey}/>
-              <_TechnologyCaption>
-                node.js
-              </_TechnologyCaption>
-            </_Technology>
-
-            <_Technology>
-              <DiRedis size='2.3rem' color={colors.coolGrey}/>
-              <_TechnologyCaption>
-                RedisDB
-              </_TechnologyCaption>
-            </_Technology>
-          </_Technologies>
+          <_LowerItemSection>
+            <_ItemTitle>
+              {'Fluxo Clicksign - Asaas'}
+            </_ItemTitle>
+            <_ItemDescription>
+              Se trata de uma integração entre a plataforma
+              de assinatura digital Clicksign e de pagamentos
+              eletrônicos Asaas
+            </_ItemDescription>
+            <_Technologies>
+              <_Technology>
+                <DiNodejsSmall size='2.3rem' color={colors.coolGrey}/>
+                <_TechnologyCaption>
+                  node.js
+                </_TechnologyCaption>
+              </_Technology>
+              <_Technology>
+                <DiRedis size='2.3rem' color={colors.coolGrey}/>
+                <_TechnologyCaption>
+                  RedisDB
+                </_TechnologyCaption>
+              </_Technology>
+            </_Technologies>
+          </_LowerItemSection>
         </_GroupItem>
         <_GroupItem>
           <Image src={Placeholder} width='100%' borderRadius='5px 5px 0 0' />
-          <_ItemTitle>
-            {'SPA React Flower Shop'}
-          </_ItemTitle>
+          <_LowerItemSection>
+            <_ItemTitle>
+              {'SPA React Flower Shop'}
+            </_ItemTitle>
+          </_LowerItemSection>
         </_GroupItem>
         <_GroupItem>
           <Image src={Placeholder} width='100%' borderRadius='5px 5px 0 0' />
+          <_LowerItemSection>
           <_ItemTitle>
-            {'Automatic invoices integration'}
+            {'Notas Fiscas Automáticas'}
           </_ItemTitle>
           <_Technology>
-              <DiMongodb size='2rem'/>
+              <DiMongodb size='2rem' color={colors.coolGrey}/>
               <_TechnologyCaption>
                 MongoDB
               </_TechnologyCaption>
             </_Technology>
+          </_LowerItemSection>
         </_GroupItem>
       </_ImageGroup>
     </_Container>
