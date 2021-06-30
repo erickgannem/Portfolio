486456cfd694218ffe046a0398ca9b34b230f286
Erick Gannem <erickgannem@gmail.com>
2021-06-30 11:25:17 -0400

    contact page finished for mobile and desktop

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,36 +2,45 @@ import React from 'react'
 import styled from 'styled-components'
 import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 
-import Container from 'components/Container'
-
 import guidelines from 'guidelines'
 
 const { colors } = guidelines
 // will be separated
 
-const _FollowMe = styled.div``
-const _GithubIcon = styled(IoLogoGithub)``
-const _LinkedinIcon = styled(IoLogoLinkedin)``
-const _InstagramIcon = styled(IoLogoInstagram)``
-
-const _SocialNetworks = styled.div`
+const _FollowMe = styled.div`
+  @media (min-width: 0) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+  }
 `
-const _SocialNetwork = styled.div`
+const _GithubIcon = styled(IoLogoGithub)`
+  color: ${colors.darkBlueGreen90};
+`
+const _LinkedinIcon = styled(IoLogoLinkedin)`
+  color: ${colors.darkBlueGreen90};
+`
+const _InstagramIcon = styled(IoLogoInstagram)`
+  color: ${colors.darkBlueGreen90};
+`
+const _SocialNetworks = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
 `
-const _SocialLink = styled.a`
-  text-decoration: none;
-  color: ${colors.darkBlueGreen};
-  font-weight: bold;
-
-  ${this}:hover {
-    cursor: pointer;
-    color: ${colors.darkBlueGreen90}
-  }
-
-  @media(max-width: 899px){
-    margin-left: 0.2rem;
+const _SocialNetwork = styled.a`
+  background: ${colors.paleGrey2};
+  border: 1px solid ${colors.coolGrey2};
+  @media (min-width: 0) {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    border-radius: 100%;
+    ${this}:nth-child(2) {
+      margin: 0 1rem;
+    }
   }
 `
 const _FieldsWrapper = styled.div`
@@ -39,20 +48,34 @@ const _FieldsWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
 `
-
 const _ContactForm = styled.form`
   background: ${colors.paleGrey2};
-  padding: 1.5rem;
+  border: 1px solid ${colors.coolGrey3};
   border-radius: 5px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media (min-width: 0) {
+    padding: 1.5rem;
+    width: 100%;    
+  }
+  @media (min-width: 500px) {
+    width: 75%;
+  }
+  @media (min-width: 700px) {
+    width: 50%;
+  }
+  @media (min-width: 1000px) {
+    width: 40%;
+  }
 `
 const _TextField = styled.input`
   padding: 1rem;
   border: none;
   border-radius: 5px;
   background: ${colors.paleGrey1};
+  outline: none;
 
   @media(max-width: 899px) {
     width: 100%;
@@ -66,6 +89,7 @@ const _TextArea = styled.textarea`
   resize: none;
   font-family: 'Open Sans';
   margin-bottom: 1rem;
+  outline: none;
 
   @media(max-width: 899px) {
     width: 100%;
@@ -78,35 +102,29 @@ const _SubmitButton = styled.button`
   border-radius: 5px;
   background-color: ${colors.azul};
   color: ${colors.white};
+  width: 6rem;
+  margin: 0 auto;
+
+  ${this}:hover {
+    cursor: pointer;
+  }
+`
+const _Container = styled.div`
+  @media (min-width: 0) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+  }
 `
 // will be separated
 
 export default function Contact () {
   return (
-    <Container
-      padding='2rem 1rem'
-    >
-      <_FollowMe>
-        <p>Muito obrigado por ter visitado meu site. Me segue nas redes:</p>
-        <_SocialNetworks>
-          <_SocialNetwork>
-            <_LinkedinIcon size='2rem'/>
-            <_SocialLink href="https://www.linkedin.com/in/erick-gannem-471840208/" target='_blank' rel="noreferrer">LinkedIn</_SocialLink>
-          </_SocialNetwork>
-          <_SocialNetwork>
-            <_InstagramIcon size='2rem'/>
-            <_SocialLink href='https://www.instagram.com/erickgannem/' target='_blank' rel="noreferrer">Instagram</_SocialLink>
-          </_SocialNetwork>
-          <_SocialNetwork>
-            <_GithubIcon size='2rem'/>
-            <_SocialLink href='https://www.github.com/erickgannem/' target='_blank' rel="noreferrer">Github</_SocialLink>
-          </_SocialNetwork>
-        </_SocialNetworks>
-      </_FollowMe>
+    <_Container>
       <_ContactForm>
-        <h2 style={{ marginBottom: '1rem', textAlign: 'center' }}>Me envie uma mensagem :)</h2>
         <_FieldsWrapper>
-          <_TextField type='text' placeholder='seu nome'/>
+          <_TextField type='text' placeholder='Seu nome'/>
           <_TextField style={{ margin: '1rem 0' }} type='text' placeholder='Seu e-mail'/>
           <_TextArea placeholder='Sua mensagem'/>
         </_FieldsWrapper>
@@ -114,6 +132,28 @@ export default function Contact () {
           Enviar
         </_SubmitButton>
       </_ContactForm>
-    </Container>
+      <_FollowMe>
+        <_SocialNetworks>
+          <_SocialNetwork
+            href='https://www.linkedin.com/in/erick-gannem-471840208/'
+            target='_blank'
+          >
+            <_LinkedinIcon size='2rem'/>
+          </_SocialNetwork>
+          <_SocialNetwork
+            href='https://www.instagram.com/erickgannem/'
+            target='_blank'
+          >
+            <_InstagramIcon size='2rem'/>
+          </_SocialNetwork>
+          <_SocialNetwork
+            href='https://www.github.com/erickgannem/'
+            target='_blank'
+          >
+            <_GithubIcon size='2rem'/>
+          </_SocialNetwork>
+        </_SocialNetworks>
+      </_FollowMe>
+    </_Container>
   )
 }
